6715c38a1d8be0d8bc800ae0c2534dbdc4a9fad2
James Talmage <james@talmage.io>
2015-11-26 00:39:35 -0500

    init commit

---
+++ cli.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+'use strict';
+
+var chalk;
+var width = 63;
+var indent;
+
+function pad(len, char) {
+	return (new Array(len)).join(char);
+}
+
+function text(text, color) {
+	var textLength = chalk.stripColor(text).length;
+	if (color) {
+		text = chalk[color](text);
+	}
+	return indent + text + pad(width - (textLength + 5), ' ') + chalk.gray('│');
+}
+
+require('fallback-cli')('ava/cli.js', function (opts) {
+	if (true || !opts.localCli) {
+		chalk = require('chalk');
+
+		indent = chalk.gray('│   ');
+
+		var blankLine = chalk.gray('│' + pad(width - 2, ' ') + '│');
+
+		console.error(
+			[
+				chalk.gray('┌' + pad(width - 2, '─') + '┐'),
+				text('ERROR: No local ' + chalk.bold('AVA') + ' installation found.', 'red'),
+				blankLine,
+				text('Run the following command in your project directory:', 'gray'),
+				blankLine,
+				text('   npm install --save-dev ava', 'blue'),
+				chalk.gray('└' + pad(width - 2, '─') + '┘')
+			].join('\n')
+		);
+
+		return process.exit(1);
+	}
+	require(opts.localCli);
+});
---
+++ readme.md
@@ -0,0 +1,24 @@
+# ava-cli
+
+> Global CLI for AVA
+
+This is a temporary fix for [AVA issue #245](https://github.com/sindresorhus/ava/issues/245).
+It is not likely to remain the permanent solution.
+
+
+## Install
+
+You must install `ava-cli` globally, and `ava` as a local dependency.
+
+```
+$ npm install -g ava-cli
+$ npm install --save-dev ava
+```
+
+## Usage
+
+Just run `ava` from inside your project directory. It will automatically find and use the locally installed version. 
+
+## License
+
+MIT © [James Talmage](http://github.com/jamestalmage)
